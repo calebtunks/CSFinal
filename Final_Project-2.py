75f24c5b52adac26407c7e64009de726f3a96c5a
calebtunks <119636260+calebtunks@users.noreply.github.com>
2024-04-25 14:08:25 -0400

    Update Final_Project-2.py

--- Final_Project-2.py
+++ Final_Project-2.py
@@ -1,30 +1,29 @@
 import string
 import random
 class PasswordManager:
-
     def __init__(self, name, master_pw):
-        self.__name = #store name of the user
-        self.__master_pw = #store master password
-        self.__login = #store an empty dictionary
-    
-    #def checker(self):
-        #return self.__passwords.copy()
+        self.__name = name
+        self.__master_pw =  master_pw
+        self.__login =  {}
+
+    def checker(self):
+        return self.__passwords.copy()
 
     def validate(self, master_pass):
         # Checks whether mp is the same as the master password
         # Returns a boolean
         # DELETE PASS AND ADD YOUR CODE BELOW THIS LINE
-        pass
+        return master_pass == self.__master_pw
 
     def get_name(self):
         # Return the name of the owner of the password manager
         # DELETE PASS AND ADD YOUR CODE BELOW THIS LINE
-        pass
+        return self.__name
 
     def get_comp_list(self):
         # Return a list of all the company names (all the keys from dictionary login, not the values)
         # DELETE PASS AND ADD YOUR CODE BELOW THIS LINE
-        pass
+        return list(self.__login.keys())
 
     def get_comp_info(self, comp_name, master_pass):
         # If master_password entered correctly
@@ -32,40 +31,34 @@ class PasswordManager:
         # Returns a tuple of username and password for the company name passed in as the argument
         # If master password is wrong, print the error message according to instructions and returns False
         # DELETE PASS AND ADD YOUR CODE BELOW THIS LINE
-        pass
+        if not self.validate(master_pass):
+            print("Incorrect master password!")
+            return False
+        return tuple(self.__login[comp_name])
 
-# ****** __generate_password begins. ******
-    #__generate_password is correct.Do not change anything in it.
 
-    def __generate_password(self, criteria = {'length':14, 'min_lower':0, 'min_upper':0, 'min_digits':0, 'min_special':0}):
-        lower_letters = string.ascii_lowercase # a string of all alphabets in lowercase
-        upper_letters = string.ascii_uppercase # a string of all alphabets in uppercase
-        digits = string.digits # a string of all numbers
-        special_chars = string.punctuation # a string of all punctuations
-        length = criteria.get('length', 14) # refer to the dictionary 'criteria' and set default required length of the password to 14
-        min_special = criteria.get('min_special', 0) # refer to the dictionary 'criteria' and set min num of speacial characters to 0
-        min_digits = criteria.get('min_digits', 0) # refer to the dictionary 'criteria' and set min num of digits to 0
-        min_upper = criteria.get('min_upper', 0) # refer to the dictionary 'criteria' and set min num of uppercase to 0
-        min_lower = criteria.get('min_lower', 0) # refer to the dictionary 'criteria' and set min num of lowercase to 0
-
-        # Ensure length requirements are met
+    def __generate_password(self,
+                            criteria={'length': 14, 'min_lower': 0, 'min_upper': 0, 'min_digits': 0, 'min_special': 0}):
+        lower_letters = string.ascii_lowercase
+        upper_letters = string.ascii_uppercase
+        digits = string.digits
+        special_chars = string.punctuation
+        length = criteria.get('length', 14)
+        min_special = criteria.get('min_special', 0)
+        min_digits = criteria.get('min_digits', 0)
+        min_upper = criteria.get('min_upper', 0)
+        min_lower = criteria.get('min_lower', 0)
         if length >= min_lower + min_upper + min_special + min_digits:
-            # Generate password with required characters
             password = (random.choices(lower_letters, k=min_lower)) + (random.choices(upper_letters, k=min_upper)) + (
                 random.choices(special_chars, k=min_special)) + (random.choices(digits, k=min_digits))
-            # Fill the remaining length with random characters
             remaining_length = length - (min_lower + min_upper + min_special + min_digits)
             password += ''.join(
                 random.choices(upper_letters + lower_letters + digits + special_chars, k=remaining_length))
-            # Shuffle the password to ensure randomness
             password_list = list(password)
             random.shuffle(password_list)
             password = ''.join(password_list)
             return password
-
-# ****** __generate_password ends. ******
-    
-    def add_data(self, comp_name, username, master_pass, criteria = {'length':14, 'min_lower':0, 'min_upper':0, 'min_digits':0, 'min_special':0}):
+    def add_data(self, comp_name, username, master_pass, criteria={'length': 14, 'min_lower': 0, 'min_upper': 0, 'min_digits': 0, 'min_special': 0}):
         # If master_password entered is correct
         # If company name not in dictionary login
         # generate a new password according to criteria by appropriately calling generate_password() method
@@ -73,10 +66,17 @@ class PasswordManager:
         # If __generate_password does not return a password, prints the error message according to instructions
         # If master password is wrong, print the error message according to instructions and return False
         # DELETE pass AND ADD YOUR CODE BELOW THIS LINE
-        pass
+        if not self.validate(master_pass):
+            print("Incorrect master password!")
+            return False
+        if comp_name in self.__login:
+            return
+        password = self.__generate_password(criteria)
+        if password is None:
+            return
+        self.__login[comp_name] = [username, password]
 
-    
-    def change_password(self, comp_name, master_pass, new_pass = None, criteria = {'length':14, 'min_lower':0, 'min_upper':0, 'min_digits':0, 'min_special':0}):
+    def change_password(self, comp_name, master_pass, new_pass=None, criteria={'length': 14, 'min_lower': 0, 'min_upper': 0, 'min_digits': 0, 'min_special': 0}):
         # If master_password entered correctly
         # If company name is present in the dictionary login
         # Updates the password to the company name
@@ -86,8 +86,18 @@ class PasswordManager:
         # If site does not exist, print the error message according to instructions
         # If master password is wrong, print the error message according to instructions and returns False
         # DELETE pass AND ADD YOUR CODE BELOW THIS LINE
-        pass
-        
+        if not self.validate(master_pass):
+            print("Incorrect master password!")
+            return False
+        if comp_name not in self.__login:
+            print("Company does not exist!")
+            return False
+        if new_pass is None:
+            new_pass = self.__generate_password(criteria)
+            if new_pass is None:
+                print("Invalid criteria!")
+                return False
+        self.__login[comp_name][1] = new_pass
 
     def remove_comp(self, comp_name, master_pass):
         # If master_password entered correctly
@@ -95,9 +105,11 @@ class PasswordManager:
         # Delete the company name and the associated data (delete item from dictionary login based on key)
         # If master password is wrong, print the error message according to instructions and returns False
         # DELETE pass AND ADD YOUR CODE BELOW THIS LINE
-        pass
-
-    
+        if not self.validate(master_pass):
+            print("Incorrect master password!")
+            return False
+        if comp_name in self.__login:
+            del self.__login[comp_name]
     def __str__(self):
         # Return a string representation of ONLY the sites
         # Should look like this
@@ -107,8 +119,5 @@ class PasswordManager:
         # Tesla
         # One company name per line with the header line
         # DELETE pass AND ADD YOUR CODE BELOW THIS LINE
-        pass
-
-
-
-
+        company_names = "\n".join(self.get_comp_list())
+        return "Company names stored for " + self.__name + " are as follows:\n" + company_names
